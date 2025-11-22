29d578b030055c0421a7cdb0c0be73231e6ec0ad
osm3rr <osm3rr@adakademy.com>
2025-11-22 16:23:48 -0400

    first commit

---
+++ js/01Variables.js
@@ -0,0 +1,15 @@
+// Declaracion de variables
+const name = "Ada";
+const lastName = "primero"
+let edad = 25;
+let genero = "Masculino";
+let isStudent = true;
+let precio = 10.99;
+
+// mostrar variables
+alert(name);
+alert(lastName);
+alert(edad);
+alert(genero);
+alert(isStudent);
+alert(precio);
