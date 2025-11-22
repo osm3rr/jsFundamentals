602b077732d3b93fd20439decd4e48588a7c17d7
osm3rr <osm3rr@adakademy.com>
2025-11-22 17:23:11 -0400

    for loops

---
+++ js/05ForLoop.js
@@ -0,0 +1,12 @@
+// bucles en javascript
+
+// bucle for
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// recorrer array
+const fruits = ["apple", "banana", "cherry"];
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
