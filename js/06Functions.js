f163c6a00124dfc33f7fab2499f31d608580b9c9
osm3rr <osm3rr@adakademy.com>
2025-11-22 17:32:16 -0400

    funciones en js

---
+++ js/06Functions.js
@@ -0,0 +1,20 @@
+// funciones en javascript
+
+// declaracion de funcion
+function sum(a, b=1) {
+    return a + b;
+}
+
+// invocacion de funcion
+console.log(sum(1, 3));
+
+// // funcion flecha
+// const sumArrow = (a, b) => a + b;
+// console.log(sumArrow(1, 2));
+
+// // funcion expression
+// const sumExpression = function (a, b) {
+//     return a + b;
+// };
+// console.log(sumExpression(1, 2));
+
