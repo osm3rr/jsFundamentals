ef3b2dcca4f3484cf9b64f597eab783423736554
osm3rr <osm3rr@adakademy.com>
2025-11-22 16:55:24 -0400

    objects in js

---
+++ js/03Object.js
@@ -0,0 +1,13 @@
+// objetos en javascript
+const person = {
+    name: "John",
+    age: 30,
+    city: "New York",
+    address: {
+        street: "123 Main St",
+        zip: "12345"
+    },
+    hobbies: ["reading", "traveling", "sports"],
+    isStudent: true,
+};
+console.log(person);
