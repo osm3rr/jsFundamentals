9e6ea910ed6ee688b40a7f8d21caf8c4e98ffb53
osm3rr <osm3rr@adakademy.com>
2025-11-22 16:37:55 -0400

    array in js

---
+++ js/02Array.js
@@ -0,0 +1,12 @@
+// arrays en javascript
+fruits = ["apple", "banana", "cherry"];
+// console.log(fruits);
+
+fruits.push("orange");
+console.log(fruits);
+
+console.log(fruits[1]);
+
+// longitud del array
+console.log(fruits.length);
+
