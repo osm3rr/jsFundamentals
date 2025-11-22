c1b4ffff5c40638a81371aaa4d9e68d8810068a6
osm3rr <osm3rr@adakademy.com>
2025-11-22 17:10:36 -0400

    condicionales en js

---
+++ js/04Conditionals.js
@@ -0,0 +1,12 @@
+// condicionales en javascript
+
+// evaluacion de edad para adulto mayor
+
+const age = 21;
+if (age >= 65) {
+    console.log("Eres adulto mayor");
+} else if (age >= 18) {
+    console.log("Eres mayor de edad");
+} else {
+    console.log("Eres menor de edad");
+}
